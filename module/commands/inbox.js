4c3305a0f0ee78dfed8b8825ff34e9a95fa9e92a
kinganik6263 <161469632+kinganik6263@users.noreply.github.com>
2024-12-30 17:56:51 +0800

    Create inbox.js

---
+++ module/commands/inbox.js
@@ -0,0 +1,23 @@
+module.exports.config = {
+    name: "inbox",           
+    description: "admin information",
+    permission: 0,          
+    prefix: true,         
+    cooldown: 5,
+}
+
+module.exports.run = async ({ api, event }) => {
+  try {
+    await api.sendMessage(`Hey I'm ${global.config.BOTNAME}`, event.senderID);
+    await api.sendMessage('check your inbox! 📥', event.threadID, event.messageID)
+  } catch (error) {
+    console.error("Error in inbox command:", error);
+    
+  api.sendMessage('আপনার ইনবক্সে নক দেওয়া যাবে না আপনি /accept কমান্ড ইউজ করেন', event.threadID, event.messageID )
+  }
+};
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Promise Rejection:', reason);
+  
+});
